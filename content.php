886f36c28679ae1402915216c0dceff7ea2094c9
Adrián Avilés Avila <aviles3a@gmail.com>
2022-04-11 19:42:19 -0700

    verificando si los comentarios estaban abiertos y el usuario podia editar los post en el content

--- content.php
+++ content.php
@@ -2,10 +2,38 @@
     <div id="<?php the_ID(); ?>" <?php post_class('page_post') ?>>
         <?php
             if( has_post_thumbnail() ){
-                the_post_thumbnail();
-            }else{
-                echo '<h1>No hay imagen</h1>';
+                ?>
+                    <div class="publication">
+                        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
+                            <?php the_post_thumbnail(); ?>
+                        </a>
+                    </div>
+                <?php
             }
         ?>
+        <a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+
+        <?php
+            if( comments_open() ){
+                ?>
+                    <div class="comment_poup">
+                        <?php 
+                            comments_popup_link();
+                        ?>
+                    </div>
+                <?php
+            }
+
+            if(current_user_can('edit_posts')){
+                ?>
+                    <div class="edit_this_post">
+                        <?php edit_post_link('Editar este Post'); ?>
+                    </div>
+                <?php
+            }else{
+                echo '<h1>No eres admin</h1>' ;
+            }
+        ?>
+
     </div>
 </div>
